bbf7c9a9acec07911ef59cf4ea0e7cb3e735f9b8
Erik Kolar <erkcarlsberg@aol.com>
2023-01-30 19:16:49 -0700

    did some fun thing with strings

--- python_strings.py
+++ python_strings.py
@@ -2,42 +2,50 @@
 #   - Create the following variables
 #   - my_first_name
 #       -set this equal to your first name
+my_first_name = 'Erik'
 #   - my_last_name
 #       -set this equal to your last name
+my_last_name = 'Kolar'
 #   - my_year_of_birth
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_year_of_birth = 1995
 #   - current_year
 #       -set this equal to 2020
-
-
-
+current_year = 2023
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
-#       - first name  
+#       - first name
+print(my_first_name)
 #       - last name
+print(my_last_name)
 #       - first letter of your first name (use the +index)
+print(my_last_name[+0])
 #       - second letter of your last name (use the -index)
+print(my_Last_name[-2)
 #       - first two letter of your first name (use the +index)
-#       - second two letter of your last name (use the -index)
-
-
-
+print(my_last_name[:])
+#       - last two letter of your last name (use the -index)
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
+print(my_first_name+my_last_name)
 #       -first name six times
-
-
+print(my_first_name + my_last_name) #concat
+print(my_first_name + ' ' + my_last_name) #concat
+print(My_first_name, my_last_name) #concat
+#       -first name six times
+print(My_first_name*6)
 
 
 
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
+print(my_first_name, my_last_name,'I was born in',my_year_of_birth)
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
+print(my_first_name, my_last_name,'I was born in',my_year_of_birth, '.',my_first_name,'enjoyed celebrating',current_year)
 
 
 # TODO Escape characters
@@ -51,3 +59,4 @@
 #       - first name and last name in lower case
 #       - length of last name
 #       - first name and last name all in upper case
+
